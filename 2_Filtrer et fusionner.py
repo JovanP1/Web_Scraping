e700573e6d451253295cda5de4166d9e701c2f18
JovanP1 <62058944+JovanP1@users.noreply.github.com>
2020-04-13 15:35:24 +0200

    Update 2_Filtrer et fusionner.py

--- 2_Filtrer et fusionner.py
+++ 2_Filtrer et fusionner.py
@@ -2,7 +2,7 @@
 # Importer la librairies os qui permet d'importer de montrer le chemin des fichiers qu'on va importer par la suite
 import os
 os.getcwd()
-os.chdir("C:\\Users\\mehdi\\Desktop\\M1 ecotr\\Langage de programmation 1\\Web scraping\\csv files")
+os.chdir("C:\\Users\\jovan\\Desktop\\M1 ecotr\\Langage de programmation 1\\Web scraping\\csv files")
 import pandas as pd
 # Importer les données 
 # sep = " ; " permet de séparer les colonnes de la data frame par colonnes du fichier csv
@@ -25,4 +25,4 @@ carizy_df = carizy_df.dropna()
 allcars_df= pd.concat([cars_df, vpauto_df, carizy_df])
 
 # L'exporter en csv 
-allcars_df.to_csv(r'C:\Users\mehdi\Desktop\M1 ecotr\Langage de programmation 1\Web scraping\csv files\Allcars.csv',index = None, header=True,sep=";")
+allcars_df.to_csv(r'C:\Users\jovan\Desktop\M1 ecotr\Langage de programmation 1\Web scraping\csv files\Allcars.csv',index = None, header=True,sep=";")
